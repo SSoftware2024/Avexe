86ac0847d70e55c6b43deff1f4a742759a77d2f6
Soluções Software <ssolucoessoftware@gmail.com>
2026-07-17 15:29:42 -0300

    feat: vuetify instalado

--- resources/Pages/Index.vue
+++ resources/Pages/Index.vue
@@ -1,4 +1,5 @@
 <template>
     <h1 class="bg-red-400">Olá inertia</h1>
     <Link href="/pg2">PG2</Link>
+    <v-btn prepend-icon="$vuetify" color="purple-darken-2"> Button </v-btn>
 </template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { Link } from '@inertiajs/vue3';
+import vuetify from './vuetify';
 
 createInertiaApp({
     resolve: name => {
@@ -10,6 +11,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
             .use(plugin)
+            .use(vuetify)
             .component('Link', Link)
             .mount(el)
     },
